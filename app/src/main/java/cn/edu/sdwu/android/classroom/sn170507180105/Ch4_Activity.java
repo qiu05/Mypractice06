d08ba700d7b471e7b02396e61b469269b936fed8
qiu05 <1272391177@qq.com>
2020-04-07 14:48:25 +0800

    2020/4/7

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180105/Ch4_Activity.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180105/Ch4_Activity.java
@@ -2,15 +2,25 @@ package cn.edu.sdwu.android.classroom.sn170507180105;
 
 import android.app.WallpaperManager;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaCodec;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class Ch4_Activity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Ch4_Activity extends AppCompatActivity implements View.OnFocusChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +29,8 @@ public class Ch4_Activity extends AppCompatActivity {
         //加载界面，不要写文件名，使用资源ID来引用资源
         setContentView(R.layout.layout_ch4_1);
         //1）获取普通界面组件:必须在setContentView之后，调用findViewById方法
-        Button button=(Button) findViewById(R.id.button1);
-        ImageView imageView=(ImageView)findViewById(R.id.ch4_iv) ;
+        Button button = (Button) findViewById(R.id.button1);
+        ImageView imageView = (ImageView) findViewById(R.id.ch4_iv);
 
         //3)调用事件源的setxxxListener方法注册事件监听器
         button.setOnClickListener(new MyClickListener());
@@ -29,25 +39,69 @@ public class Ch4_Activity extends AppCompatActivity {
         imageView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-               //设置壁纸
-                WallpaperManager wallpaperManager=(WallpaperManager) getSystemService(WALLPAPER_SERVICE);
+                //设置壁纸
+                WallpaperManager wallpaperManager = (WallpaperManager) getSystemService(WALLPAPER_SERVICE);
                 try {
                     wallpaperManager.setResource(R.raw.jub);
-                }catch (Exception e){
-                    Log.e(Ch4_Activity.class.toString(),e.toString());
+                } catch (Exception e) {
+                    Log.e(Ch4_Activity.class.toString(), e.toString());
                 }
 
                 return true;
             }
         });
+
+        EditText email = (EditText) findViewById(R.id.ch4_1_email);
+        email.setOnFocusChangeListener(this);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ch4_1_11);
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                float x = motionEvent.getX();
+                float y = motionEvent.getY();
+                TextView textView = (TextView) findViewById(R.id.ch4_1_tv);
+                textView.setText("x:"+x+",y:"+y);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean b) {
+        //参数b代表是否获取焦点
+        //判断邮箱地址的合法性
+        EditText editText = (EditText) view;
+        if (!b) {
+            String content = editText.getText().toString();//得到EditText的内容
+            String regEx1 = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+            Pattern pattern = Pattern.compile(regEx1);
+            Matcher matcher = pattern.matcher(content);
+            TextView textView = (TextView) findViewById(R.id.ch4_1_tv);
+            if (matcher.matches()) {
+                textView.setText("");
+            } else {
+                textView.setText("email invalidate");
+            }
+        }
     }
 
     //2）实现事件监听类
-    class MyClickListener implements View.OnClickListener{
+    class MyClickListener implements View.OnClickListener {
 
-        public void onClick(View view){
-            Log.i(Ch4_Activity.class.toString(),"button click");
+        public void onClick(View view) {
+
+            Log.i(Ch4_Activity.class.toString(), "button click");
         }
 
+
     }
+
+    public  void startMain(View view){
+        //界面跳转
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180105/Ch5Activity1.java
@@ -0,0 +1,95 @@
+package cn.edu.sdwu.android.classroom.sn170507180105;
+
+import android.animation.AnimatorSet;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Ch5Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ch5_1);
+    }
+    public void toast1(View view){
+        Toast.makeText(this,"toast1",Toast.LENGTH_LONG).show();
+    }
+    public void toast2(View view){
+        Toast toast=Toast.makeText(this,"toast2",Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
+    public void toast3(View view){
+        //加载自定义的布局文件，将其转换为一个View类的实例
+        LayoutInflater layoutInflater=getLayoutInflater();
+        View view1=layoutInflater.inflate(R.layout.layout_toast,null);
+
+        //设置文本
+        TextView textView=(TextView) view1.findViewById(R.id.toast_tv);
+        textView.setText("toast3");
+
+        Toast toast=new Toast(this);
+        toast.setView(view1);//设置界面
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
+
+    public void notification(View view){
+        Notification.Builder builder=new Notification.Builder(this);
+        //在构造器中，完成通知各种属性的设置
+        builder.setContentText("title");
+        builder.setContentText("message");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setSmallIcon(android.R.drawable.ic_input_delete);//引用系统资源
+        //调用build方法，完成构造
+        Notification notification=builder.build();
+        //使用NotificManage，发送通知
+        NotificationManager notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(101,notification);
+
+    }
+    public void cancel_notification(View view){
+        //使用NotificManage取消通知
+        NotificationManager notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(101);
+    }
+    public void alert_dialog(View view){
+        AlertDialog.Builder builder=new  AlertDialog.Builder(this);
+        //在构造器中
+        builder.setTitle("title");
+        builder.setMessage("message");
+        builder.setIcon(android.R.drawable.ic_dialog_email);
+        builder.setPositiveButton("confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //添加点击按钮后的，处理
+                Toast.makeText(Ch5Activity1.this,"confirm",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("exit",new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch5Activity1.this,"exit",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNeutralButton("cancel", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch5Activity1.this,"cancel",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
+    }
+}
